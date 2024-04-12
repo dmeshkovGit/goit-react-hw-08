ede9101a2f073fe69ab1a07032a1445a499b54db
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2024-04-12 12:35:41 +0300

    fixed

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,9 +1,19 @@
 import { NavLink } from 'react-router-dom'
 import css from './AuthNav.module.css'
 import { FiLogIn } from "react-icons/fi";
+import { MdAppRegistration } from "react-icons/md";
+import clsx from 'clsx';
 
 export default function AuthNav() {
+
+    const makeLinkClass= ({ isActive }) => {
+        return clsx(css.navLink, isActive && css.isActive)
+    };
+
     return (
-        <><NavLink to='/login'><FiLogIn className={css.logInIcon} /></NavLink></>
+        <div className={css.authNavWrapper}>
+            <NavLink className={makeLinkClass} to='/register'><MdAppRegistration className={css.regIcon} /></NavLink>
+            <NavLink className={makeLinkClass} to='/login'><FiLogIn className={css.logInIcon} /></NavLink>
+        </div>
     )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,20 @@
 import css from './Navigation.module.css'
 import { FaCircleInfo } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom'
+import { BiSolidContact } from "react-icons/bi";
+import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
 export default function Navigation() {
-
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const makeLinkClass = ({ isActive }) => {
+        return clsx(css.navLink, isActive && css.isActive, !isLoggedIn && css.soloLink)
+    };
     return (
-        <div><NavLink to="/" className={css.homeLink}><FaCircleInfo className={css.appIcon} /></NavLink></div>
+        <nav className={css.authNavWrapper}>
+            <NavLink to="/" className={makeLinkClass}><FaCircleInfo className={css.appIcon} /></NavLink>
+            {isLoggedIn && <NavLink to="/contacts" className={makeLinkClass}><BiSolidContact className={css.contactsIcon} /></NavLink>}
+        </nav>
     )
 }
